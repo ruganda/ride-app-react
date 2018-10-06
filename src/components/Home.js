b1217d042c1595fcda194ab95ad76169bbebc02e
ruganda <mubaruganda@gmail.com>
2018-10-07 00:23:58 +0300

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage =()=>{
+  return(
+      <section id="showcase">
+          <div className="jumbotron">
+              <h1>RIDE MY WAY</h1>
+              <p>Ride-my App is a carpooling application that provides drivers </p>
+              <p>with the ability to create ride offers and passengers </p>
+              <p>to join available ride offers</p>
+          </div>
+      </section>
+  );
+};
+
+export default HomePage
